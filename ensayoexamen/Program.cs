71c46e1a506a4055a5929ef4517f082c501527c4
SantiagoYpes <santiyezu8@hotmail.com>
2026-02-24 21:31:14 -0500

    proyecto

---
+++ ensayoexamen/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+public class Libros {
+
+public int Id {get ;set;}
+public String? Nombre {get ;set;}
+public int Nhojas {get ;set;}
+public Decimal Precio {get ;set;} = 0.0m  ;
+
+}
+
+
+public class Librerias {
+ public int Id ;
+ public String? Nombre {get ;set;}
+ public String? Direccion {get;set;}
+ public DateTime fecha { get ;set ;}
+ public List <Libros>? Libros {get ;set;}
+ 
+}
